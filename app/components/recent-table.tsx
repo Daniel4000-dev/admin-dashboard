17f1c5129d273b2264950ae45ce83e907d40aebd
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2024-04-18 00:34:48 +0100

    .

--- app/components/recent-table.tsx
+++ app/components/recent-table.tsx
@@ -1,16 +1,36 @@
 "use client"
 
 import * as React from "react"
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
 const data: Sales[] = [
   {
     id: "m5gr84i9",
     product: "Laptop",
+    quantity: 2,
     amount: 55000,
-    qauntity: 2,
     status: "success",
   },
 
