bdb5da70ed381bb5e120f8562917d44740a151ef
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2024-05-04 14:01:56 +0100

    .

--- components/ui/theme-switcher.tsx
+++ components/ui/theme-switcher.tsx
@@ -8,12 +8,34 @@ export function ThemeSwitcher(){
     return (
        <DropdownMenu>
         <DropdownMenuTrigger asChild>
-            <Button variant='outline' size='icon' className='rounded-full p-0 bg-foreground hover:bg-muted text-background hover:text-muted-foreground border-0 outline-none'>
-                <Palette className='w-6 h-6'></Palette>
-            </Button>
-            <DropdownMenuContent>
+            <div>
+                <Button variant='outline' size='icon' className='rounded-full p-0 bg-foreground hover:bg-muted text-background hover:text-muted-foreground border-0 outline-none'>
+                    <Palette className='w-6 h-6'></Palette>
+                </Button>
+        
+                <DropdownMenuContent>
+                    <DropdownMenuItem onClick={() => setTheme('light')}>
+                        <span>Light</span>
+                        {theme === 'light' && <CheckIcon
+                        className='ml-2 h-4 w-4'
+                        />}
+                    </DropdownMenuItem>
 
-            </DropdownMenuContent>
+                    <DropdownMenuItem onClick={() => setTheme('dark')}>
+                        <span>Dark</span>
+                        {theme === 'dark' && <CheckIcon
+                        className='ml-2 h-4 w-4'
+                        />}
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem onClick={() => setTheme('custom')}>
+                        <span>Custom</span>
+                        {theme === 'custom' && <CheckIcon
+                        className='ml-2 h-4 w-4'
+                        />}
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </div>
         </DropdownMenuTrigger>
        </DropdownMenu>
     )
